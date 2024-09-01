fb3fad87e0e4b13afb2552fbdd7ca174d48a5343
Ariel <127093985+ariel3007@users.noreply.github.com>
2024-09-01 14:23:47 +0300

    Initial commit - convert to kotlin

---
+++ src/main/java/me/olios/plugins/simplecompressor/SimpleCompressor.kt
@@ -0,0 +1,9 @@
+package me.olios.plugins.simplecompressor
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SimpleCompressor : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+}
